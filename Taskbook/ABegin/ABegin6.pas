438876be567e5ea0411a213e862c554a3ac00931
Sewadius <vertnather@gmail.com>
2024-03-12 15:18:02 +0300

    6 & 7 tasks added

---
+++ Taskbook/ABegin/ABegin6.pas
@@ -0,0 +1,10 @@
+﻿uses PT4;
+
+begin
+  Task('ABegin6');
+  var (a, b, c) := ReadReal3;
+  var volume: real = a * b * c;
+  var s := 2 * (a * b + b * c + a * c);
+  
+  Println(volume, s)
+end.
---
+++ Taskbook/ABegin/ABegin7.pas
@@ -0,0 +1,13 @@
+﻿uses PT4;
+
+const PI = 3.14;
+
+begin
+  Task('ABegin7');
+  var r := ReadReal;
+  
+  var l := 2 * PI * r;
+  var s := PI * Sqr(r);
+  
+  Println(l, s);
+end.
