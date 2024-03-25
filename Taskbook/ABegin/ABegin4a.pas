ce780a5156b392691f38e2ca80722db7cb826bdd
Sewadius <vertnather@gmail.com>
2024-03-25 16:48:06 +0300

    new solved tasks added

---
+++ Taskbook/ABegin/ABegin4a.pas
@@ -0,0 +1,7 @@
+﻿uses PT4;
+{ решение через последовательность }
+
+begin
+  Task('ABegin4');
+  ReadSeqReal(1).Select(t → 3.14 * t).Print
+end.
---
+++ Taskbook/ABegin/ABegin5a.pas
@@ -0,0 +1,9 @@
+﻿uses PT4;
+{ решение через последовательность }
+
+begin
+  Task('ABegin5');
+  var s: sequence of real := ReadSeqReal(1);
+  s.Select(t → t ** 3).Print;
+  s.Select(t → 6 * t ** 2).Print
+end.
